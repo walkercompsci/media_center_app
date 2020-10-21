c7e218afd5d385e02fdc1d676a54f88cd5eb92f0
hobbsgag000 <hobbsgag000@warren.k12.in.us>
2020-10-21 10:57:21 -0400

    added ninjaID and quoteCard projects to pull from and updated flutter by typing flutter channel beta flutter update

--- lib/qc_main.dart
+++ lib/qc_main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'quote.dart';
+import 'quote_card_2questionmark.dart';
 
 void main() => runApp(MaterialApp(
   home: QuoteList(),
@@ -18,43 +19,25 @@ class _QuoteListState extends State<QuoteList> {
     Quote(author: 'owie', text: 'have a nice daygggggggggggggg'),
   ];
 
-  Widget quoteTemplate(quote){
-    return Card(
-      margin: EdgeInsets.fromLTRB(16.0, 16.0, 26.0, 0),
-      child: Column(
-        children: <Widget>[
-          Text(
-            quote.text,
-            style: TextStyle(
-              fontSize:18.0,
-              color:Colors.grey[600],
-            )
-          ),
-          SizedBox(height: 6.0),
-          Text(
-            quote.author,
-            style: TextStyle(
-              fontSize: 14.0,
-              color: colors.grey[800]
-            )
-          )
-        ],
-      ),
-    );
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[200],
-      appBar: AppBar(
-        title: Text('Awesome Quotes'),
-        centerTitle: true,
-        backgroundColor: Colors.redAccent,
-      ),
-      body: Column(
-        children: quotes.map((quote) =>quoteTemplate(quote)).toList(),
-      )
+        backgroundColor: Colors.grey[200],
+        appBar: AppBar(
+          title: Text('Awesome Quotes'),
+          centerTitle: true,
+          backgroundColor: Colors.redAccent,
+        ),
+        body: Column(
+          children: quotes.map((quote) =>QuoteCard(
+              quote: quote,
+              delete: () {
+                setState(() {
+                  quotes.remove(quote);
+                });
+              }
+          )).toList(),
+        )
     );
   }
 }
--- lib/quote_card.dart
+++ lib/quote_card.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'quote.dart';
 
 class QuoteCard extends StatelessWidget {
 
   final Quote quote;
-  QuoteCard({ this.quote });
+  final Function delete;
+  QuoteCard({ this.quote, this.delete });
 
   @override
   Widget build(BuildContext context) {
@@ -29,10 +31,10 @@ class QuoteCard extends StatelessWidget {
                 color: Colors.grey[800]
               )
             ),
-            SizedBox(height:8.0),
+            SizedBox(height: 8.0),
             FlatButton.icon(
-              onPressed: () {},
-                label: Text('delete quote'),
+              onPressed: delete,
+              label: Text('delete quote'),
               icon: Icon(Icons.delete),
             )
           ],
